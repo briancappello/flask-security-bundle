0f4a90d40ebe40c65879df1b8fbb4ecd810d1f77
Brian Cappello <briancappello@gmail.com>
2018-04-08 10:40:48 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
-def read_requirements(filename):
-    def is_pkg(line):
-        return line and not line.startswith(('--', 'git', '#'))
-
-    with open(filename, encoding='utf-8') as f:
-        return [line for line in f.read().splitlines() if is_pkg(line)]
-
-
 setup(
     name='Flask Security Bundle',
     version='0.1.0',
@@ -24,13 +16,30 @@ setup(
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=find_packages(include=['flask_security_bundle']),
-    install_requires=read_requirements('requirements.txt'),
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'bcrypt>=3.1.3',
+        'flask-controller-bundle>=0.2.0',
+        'flask-security>=3.0.0',
+        'flask-sqlalchemy-bundle>=0.2.0',
+        'flask-unchained>=0.2.0',
+    ],
+    extras_require={
+        'dev': [
+            'coverage',
+            'factory_boy',
+            'pytest',
+            'pytest-flask',
+            'tox',
+        ],
+    },
     include_package_data=True,
     zip_safe=False,
     entry_points={
