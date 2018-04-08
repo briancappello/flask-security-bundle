4cccb3d1687a59c79b9b02bd175241d2160a107c
Brian Cappello <briancappello@gmail.com>
2018-04-08 10:41:50 -0400

    release boilerplate

--- flask_security_bundle/__init__.py
+++ flask_security_bundle/__init__.py
@@ -1,3 +1,16 @@
+"""
+    flask_security_bundle
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Authentication and authorization support for Flask Unchained
+
+    :copyright: Copyright © 2018 Brian Cappello
+    :license: MIT, see LICENSE for more details
+"""
+
+__version__ = '0.1.0'
+
+
 from flask_unchained import Bundle
 
 from .decorators import (
