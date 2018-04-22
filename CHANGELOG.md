84e37906c71eaa49464dc724e5249f0e3b25a86a
Brian Cappello <briancappello@gmail.com>
2018-04-22 13:51:30 -0600

    depend on flask-mail-bundle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.1 (2018/04/22)
+
+* depend on flask-mail-bundle
+
 ## 0.1.0 (2018/04/08)
 
 * initial release
--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=[
         'bcrypt>=3.1.3',
         'flask-controller-bundle>=0.2.0',
+        'flask-mail-bundle>=0.2.0',
         'flask-security>=3.0.0',
         'flask-sqlalchemy-bundle>=0.2.0',
         'flask-unchained>=0.2.0',
