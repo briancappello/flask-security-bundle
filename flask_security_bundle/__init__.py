9ddf76a91567f01158ec30a6eb2b3ba1096eb7b1
Brian Cappello <briancappello@gmail.com>
2018-07-31 09:17:55 -0600

    configure a request hook to automatically set the csrf token in the response cookie

--- flask_security_bundle/__init__.py
+++ flask_security_bundle/__init__.py
@@ -11,7 +11,7 @@
 __version__ = '0.3.0'
 
 
-from flask_security import current_user
+from flask_security import current_user  # alias this here
 from flask_unchained import Bundle
 
 from .decorators import (
@@ -26,3 +26,14 @@ from .views import SecurityController, UserResource
 class FlaskSecurityBundle(Bundle):
     blueprint_names = []
     command_group_names = ['users', 'roles']
+
+    @classmethod
+    def after_init_app(cls, app):
+        from flask_wtf.csrf import generate_csrf
+
+        # send CSRF token in the cookie
+        @app.after_request
+        def set_csrf_cookie(response):
+            if response:
+                response.set_cookie('csrf_token', generate_csrf())
+            return response
