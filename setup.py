23d58936e3f1fab83a3eb24b8babe14f6d9ccbd3
Brian Cappello <briancappello@gmail.com>
2018-07-31 09:16:29 -0600

    fix pytest imports from flask-unchained

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         'bcrypt>=3.1.3',
         'flask-security>=3.0.0',
-        'flask-unchained>=0.5.0',
+        'flask-unchained>=0.5.1',
     ],
     extras_require={
         'dev': [
