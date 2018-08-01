2bdb028bd534656b0ebd99a46b378327cc3b8236
Brian Cappello <briancappello@gmail.com>
2018-08-01 01:03:49 -0600

    reorganize config.py

--- flask_security_bundle/config.py
+++ flask_security_bundle/config.py
@@ -16,57 +16,18 @@ from .models import AnonymousUser
 
 class Config:
     SECRET_KEY = 'change_me_not_secret_key'
-    SECURITY_PASSWORD_SALT = 'security-password-salt'
 
-    SECURITY_CONFIRMABLE = False
-    SECURITY_REGISTERABLE = False
-    SECURITY_RECOVERABLE = False
-    SECURITY_TRACKABLE = False
-    SECURITY_CHANGEABLE = False
-
-    SECURITY_LOGIN_WITHIN = '1 days'
-    SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
-    SECURITY_DEFAULT_REMEMBER_ME = False
-    SECURITY_CONFIRM_EMAIL_WITHIN = '5 days'
-    SECURITY_RESET_PASSWORD_WITHIN = '5 days'
     SECURITY_TOKEN_AUTHENTICATION_KEY = 'auth_token'
     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
     SECURITY_TOKEN_MAX_AGE = None
-    SECURITY_UNAUTHORIZED_CALLBACK = lambda: abort(HTTPStatus.UNAUTHORIZED)
 
-    # FIXME-identity
-    SECURITY_USER_IDENTITY_ATTRIBUTES = ['email']
-
-    SECURITY_LOGIN_FORM = LoginForm
-    SECURITY_CONFIRM_REGISTER_FORM = ConfirmRegisterForm
-    SECURITY_REGISTER_FORM = RegisterForm
-    SECURITY_FORGOT_PASSWORD_FORM = ForgotPasswordForm
-    SECURITY_RESET_PASSWORD_FORM = ResetPasswordForm
-    SECURITY_CHANGE_PASSWORD_FORM = ChangePasswordForm
-    SECURITY_SEND_CONFIRMATION_FORM = SendConfirmationForm
     SECURITY_ANONYMOUS_USER = AnonymousUser
-
-    SECURITY_POST_LOGIN_REDIRECT_ENDPOINT = '/'
-    SECURITY_POST_LOGOUT_REDIRECT_ENDPOINT = '/'
-    SECURITY_CONFIRM_ERROR_REDIRECT_ENDPOINT = None
-    SECURITY_POST_REGISTER_REDIRECT_ENDPOINT = None
-    SECURITY_POST_CONFIRM_REDIRECT_ENDPOINT = None
-    SECURITY_POST_RESET_REDIRECT_ENDPOINT = None
-    SECURITY_POST_CHANGE_REDIRECT_ENDPOINT = None
-
-    SECURITY_FORGOT_PASSWORD_ENDPOINT = 'security_controller.forgot_password'
-    SECURITY_API_RESET_PASSWORD_HTTP_GET_REDIRECT = None
-    SECURITY_INVALID_RESET_TOKEN_REDIRECT = 'security_controller.forgot_password'
-    SECURITY_EXPIRED_RESET_TOKEN_REDIRECT = 'security_controller.forgot_password'
-
-    SECURITY_SEND_REGISTER_EMAIL = True
-    SECURITY_SEND_PASSWORD_CHANGE_EMAIL = True
-    SECURITY_SEND_PASSWORD_RESET_EMAIL = True
-    SECURITY_SEND_PASSWORD_RESET_NOTICE_EMAIL = True
+    SECURITY_UNAUTHORIZED_CALLBACK = lambda: abort(HTTPStatus.UNAUTHORIZED)
 
     # make datetimes timezone-aware by default
     SECURITY_DATETIME_FACTORY = lambda: datetime.now(timezone.utc)
 
+    SECURITY_PASSWORD_SALT = 'security-password-salt'
     SECURITY_PASSWORD_HASH = 'bcrypt'
     SECURITY_PASSWORD_SINGLE_HASH = False
     SECURITY_PASSWORD_SCHEMES = ['argon2',
@@ -77,6 +38,66 @@ class Config:
     SECURITY_DEPRECATED_PASSWORD_SCHEMES = ['auto']
     SECURITY_HASHING_SCHEMES = ['sha512_crypt']
     SECURITY_DEPRECATED_HASHING_SCHEMES = []
+
+    # login / logout
+    # ==============
+    SECURITY_LOGIN_FORM = LoginForm
+    SECURITY_DEFAULT_REMEMBER_ME = False
+    SECURITY_USER_IDENTITY_ATTRIBUTES = ['email']  # FIXME-identity
+    SECURITY_POST_LOGIN_REDIRECT_ENDPOINT = '/'
+    SECURITY_POST_LOGOUT_REDIRECT_ENDPOINT = '/'
+
+    # registration
+    # ============
+    SECURITY_REGISTERABLE = False
+    SECURITY_REGISTER_FORM = RegisterForm
+    SECURITY_CONFIRM_REGISTER_FORM = ConfirmRegisterForm
+    SECURITY_POST_REGISTER_REDIRECT_ENDPOINT = None
+    SECURITY_SEND_REGISTER_EMAIL = True
+
+    # email confirmation required functionality
+    # =========================================
+    SECURITY_CONFIRMABLE = False
+    """Enable required email confirmation for new users."""
+
+    SECURITY_SEND_CONFIRMATION_FORM = SendConfirmationForm
+    """Form class to use for the re-send confirmation email form."""
+
+    SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
+    """Allow users to login without confirming their email first."""
+
+    SECURITY_CONFIRM_EMAIL_WITHIN = '5 days'
+    """
+    How long to wait until considering the token in confirmation emails to be expired.
+    """
+
+    SECURITY_POST_CONFIRM_REDIRECT_ENDPOINT = None
+    SECURITY_CONFIRM_ERROR_REDIRECT_ENDPOINT = None
+
+    # change password functionality
+    # =============================
+    SECURITY_CHANGEABLE = False
+    SECURITY_CHANGE_PASSWORD_FORM = ChangePasswordForm
+    SECURITY_POST_CHANGE_REDIRECT_ENDPOINT = None
+    SECURITY_SEND_PASSWORD_CHANGED_EMAIL = True
+
+    # forgot password functionality
+    # =============================
+    SECURITY_RECOVERABLE = False
+    SECURITY_FORGOT_PASSWORD_FORM = ForgotPasswordForm
+
+    # reset password
+    # --------------
+    SECURITY_RESET_PASSWORD_FORM = ResetPasswordForm
+    SECURITY_RESET_PASSWORD_WITHIN = '5 days'
+    SECURITY_POST_RESET_REDIRECT_ENDPOINT = None
+    SECURITY_INVALID_RESET_TOKEN_REDIRECT = 'security_controller.forgot_password'
+    SECURITY_EXPIRED_RESET_TOKEN_REDIRECT = 'security_controller.forgot_password'
+    SECURITY_API_RESET_PASSWORD_HTTP_GET_REDIRECT = None
+    SECURITY_SEND_PASSWORD_RESET_NOTICE_EMAIL = True
+
+    # trackable: record login stats, IPs, etc
+    SECURITY_TRACKABLE = False
 
 
 class TestConfig:
